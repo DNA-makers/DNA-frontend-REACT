2a6018924a1cefd55032c4242fa3b917755d4d13
Jasper Samson <s1136647@student.hsleiden.nl>
2023-06-12 12:36:43 +0200

    navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,47 @@
-import React from "react";
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navigation.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faFont, faCircleHalfStroke } from '@fortawesome/free-solid-svg-icons';
+import ImageComponent from "./ImageComponent";
+import Nav_logo from "../img/navbar-logo.png";
 
 const Navigation = () => {
-    return (
-        <nav>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/landingsPage">LandingsPage</Link></li>
-            </ul>
-        </nav>
-    );
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const handleDropdownToggle = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  return (
+    <nav>
+      <div className="navbar-container">
+        <ul className="menu-items">
+          <li className="">
+            <Link to="/">
+              <ImageComponent src={Nav_logo} alt={"MijnDNAmedicatiepas"} style={{  height: "80px" }}/>
+            </Link>
+          </li>
+        </ul>
+        <ul className="menu-items">
+          <li><Link to="/landingsPage">Overzicht</Link></li>
+          <li><Link to="/landingsPage">Over Ons</Link></li>
+          <li><Link to="/landingsPage">FAQ</Link></li>
+          <li className="dropdown-item">
+            <button className="dropdown-toggle" onClick={handleDropdownToggle}>
+              <FontAwesomeIcon icon={faBars} />
+            </button>
+            {isDropdownOpen && (
+              <ul className="dropdown-menu">
+                <li><FontAwesomeIcon icon={faFont} /></li>
+                <li><FontAwesomeIcon icon={faCircleHalfStroke} /></li>
+              </ul>
+            )}
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navigation;
