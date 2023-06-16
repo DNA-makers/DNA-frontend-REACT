e6a2e9a171ad138750a3444a4f33b20c190fe222
Jelmer van Ofwegen <offie1@outlook.com>
2023-06-17 00:08:06 +0200

    Added 404 pagina

---
+++ src/components/NotFound.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import "./NotFound.css";
+
+const NotFound = () => {
+  return (
+    <div className='notFoundContainer'>
+      <h1>404</h1>
+      <h2>Page Not Found</h2>
+      <p>De pagina die u probeert te bereiken bestaat niet.</p>
+      <Link to="/landingsPage">
+        <button className='returnButton'>Return home</button>
+      </Link>
+      
+    </div>
+  );
+};
+
+export default NotFound;
