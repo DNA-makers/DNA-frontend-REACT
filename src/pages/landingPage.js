e0edd2010fcb3eb2d05648247d775da7880811aa
wimb64 <wimb64@icloud.com>
2023-06-20 01:31:44 +0200

    landingspage buttons link to correct pages

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -4,22 +4,32 @@ import ImageComponent from "../components/ImageComponent";
 import logo from "../logo1.svg";
 import LUMC_logo from "../img/LUMC_logo.png";
 import AMC_logo from "../img/AMC_logo.png";
-import logo_maasstad from "../img/logo-maasstad.png"
+import logo_maasstad from "../img/logo-maasstad.png";
 import ButtonComponent from "../components/ButtonComponent";
 
 function LandingPage() {
   return (
     <div className="landing-page">
       <div className="center">
-        <ImageComponent src={logo} alt="logo van MijnDNAmedicatiepas"/>
+        <ImageComponent src={logo} alt="logo van MijnDNAmedicatiepas" />
         <h1>MijnDNAmedicatiepas</h1>
         <section className="action-buttons">
-          <ButtonComponent text="Informatie voor patienten" className="patient-button"/>
-          <ButtonComponent text="Informatie voor zorgverleners" className="caretaker-button"/>
+          <ButtonComponent
+            text="Informatie voor patienten"
+            className="patient-button"
+            link="/medAdvice"
+          />
+          <ButtonComponent
+            text="Informatie voor zorgverleners"
+            className="caretaker-button"
+            link="/"
+          />
         </section>
       </div>
       <section className="partners-section">
-        <h2 className="partners-heading">Deze website wordt mede mogelijk gemaakt door:</h2>
+        <h2 className="partners-heading">
+          Deze website wordt mede mogelijk gemaakt door:
+        </h2>
         <div className="partners">
           <ImageComponent src={LUMC_logo} />
           <ImageComponent src={AMC_logo} />
